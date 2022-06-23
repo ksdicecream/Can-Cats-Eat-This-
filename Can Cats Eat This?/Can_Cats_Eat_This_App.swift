d7699ec5ed576a412cccd775237e001c3ddbd66f
ksdicecream <ksdicecream@gmail.com>
2022-06-23 12:35:35 -0700

    Initial Commit

---
+++ Can Cats Eat This?/Can_Cats_Eat_This_App.swift
@@ -0,0 +1,17 @@
+//
+//  Can_Cats_Eat_This_App.swift
+//  Can Cats Eat This?
+//
+//  Created by Kimai on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Can_Cats_Eat_This_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
